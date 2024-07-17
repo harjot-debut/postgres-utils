64056f93e4a321dc7bc033e01044ddf14dccc4ce
harjot-debut <harjot.singh@debutinfotech.com>
2024-07-17 16:26:51 +0530

    added dockefile for cronjob

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 # s
 ./create-uploads-dir.sh
 
-docker pull hs60/docker-test1:latest 
+# docker pull hs60/docker-test1:latest 
 
-docker pull hs60/docker-test2:latest 
+# docker pull hs60/docker-test2:latest 
 
 docker-compose -f docker-compose.yaml up -d
